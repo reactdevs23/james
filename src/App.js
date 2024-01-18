2fcace4b93bd63cb16a51fea8076d74660b9e238
dalimkumar604 <dalimkumar212582@gmail.com>
2024-01-18 23:15:48 +0600

    gifResize

--- src/App.js
+++ src/App.js
@@ -5,39 +5,19 @@ import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
 import JamesKnownFrom from "./components/JamesKnownFrom/JamesKnownFrom";
 import LookingFor from "./components/LookingFor/LookingFor";
-import NoTax from "./components/NoTax/NoTax";
+
 import Roadmap from "./components/Roadmap/Roadmap";
-import Loader from "./components/Loader/Loader";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    let timeid = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    return () => {
-      clearTimeout(timeid);
-    };
-  }, []);
   return (
     <>
-      <>
-        {loading ? (
-          <Loader />
-        ) : (
-          <>
-            <Header />
-            <HeroSection />
-            <LookingFor />
-            <CareFul />
-            <Roadmap />
+      <Header />
+      <HeroSection />
+      <LookingFor />
+      <CareFul />
+      <Roadmap />
 
-            <JamesKnownFrom />
-          </>
-        )}
-      </>
+      <JamesKnownFrom />
     </>
   );
 }
